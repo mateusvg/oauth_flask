fefdab4fbe69a7d2e5bb8e608aab45a2d0ee2283
Mateus Vitorino Guimaraes <mateusvitorino@gmail.com>
2025-01-08 18:30:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,5 +10,9 @@ def home():
 def redirect_to_home():
     return redirect(url_for('home'))  # Redireciona para a rota 'home'
 
+@app.route('/redirect-to-google', methods=['GET'])
+def redirect_to_google():
+    return redirect('https://www.google.com')  # Redireciona para a rota 'home'
+
 if __name__ == '__main__':
     app.run(debug=True)
