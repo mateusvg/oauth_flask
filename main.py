f98bd7c4a293d780a4d00e7447769ab3f0128fcc
Mateus Vitorino Guimaraes <mateusvitorino@gmail.com>
2025-01-08 18:29:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/home', methods=['GET'])
 def home():
     return "Bem-vindo à página inicial!"
 
-@app.route('/redirect-to-home')
+@app.route('/redirect-to-home', methods=['GET'])
 def redirect_to_home():
     return redirect(url_for('home'))  # Redireciona para a rota 'home'
 
