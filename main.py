733700e88e5484a3882459773f8dd8d39910c5b4
Mateus Vitorino Guimaraes <mateusvitorino@gmail.com>
2025-01-08 16:58:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return '<h1>Home Page</h1>'
+@app.route('/post', methods=['POST'])
+def post_example():
+    data = request.get_json()  # Pega os dados JSON do corpo da requisição
+    if data:
+        return jsonify({"message": "Dados recebidos", "data": data}), 200
+    else:
+        return jsonify({"message": "Nenhum dado enviado"}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
