a55e9932c72b5c93f7234085cd48a8e92a6a0721
Mateus Vitorino Guimaraes <mateusvitorino@gmail.com>
2025-01-08 18:23:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,14 @@
-import logging
-from flask import Flask, request, jsonify, Response
-
-# Configuração do logger
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
-@app.before_request
-def log_request_info():
-    logger.debug(f'Requisição recebida: {request.method} {request.url}')
-    logger.debug(f'Cabeçalhos: {request.headers}')
-    if request.data:
-        logger.debug(f'Dados recebidos: {request.data}')
+@app.route('/home')
+def home():
+    return "Bem-vindo à página inicial!"
 
-@app.after_request
-def log_response_info(response):
-    logger.debug(f'Resposta enviada: {response.status}')
-    logger.debug(f'Cabeçalhos da resposta: {response.headers}')
-    return response
-
-@app.route('/post', methods=['POST'])
-def post_example():
-    data = request.form  # Pega os dados do corpo da requisição, assumindo que são enviados como formulário
-    if data:
-        logger.info(f'Dados recebidos: {data}')
-        response = jsonify({"message": "Dados recebidos", "data": data})
-        response.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-        return response, 200
-    else:
-        logger.warning(f'Erro: Nenhum dado enviado. Dados recebidos: {request.data}')
-        response = jsonify({"message": "Nenhum dado enviado"})
-        response.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-        return response, 400
+@app.route('/redirect-to-home')
+def redirect_to_home():
+    return redirect(url_for('home'))  # Redireciona para a rota 'home'
 
 if __name__ == '__main__':
     app.run(debug=True)
