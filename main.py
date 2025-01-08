f2297f79a8776586a4d67b2b11af222f9cca548b
Mateus Vitorino Guimaraes <mateusvitorino@gmail.com>
2025-01-08 17:07:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import logging
 from flask import Flask, request, jsonify
+
+# Configuração do logger
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
@@ -8,6 +13,7 @@ def post_example():
     if data:
         return jsonify({"message": "Dados recebidos", "data": data}), 200
     else:
+        logger.error("Erro: Nenhum dado enviado. Dados recebidos: %s", request.data)
         return jsonify({"message": "Nenhum dado enviado"}), 400
 
 if __name__ == '__main__':
