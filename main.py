fea2f3bc510938a72f6db5a22d0f121f0d6148d4
Mateus Vitorino Guimaraes <mateusvitorino@gmail.com>
2025-01-08 17:13:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import logging
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 
 # Configuração do logger
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-# Configura o log para capturar tudo
 app = Flask(__name__)
 
 @app.before_request
 def log_request_info():
-    # Loga todos os detalhes da requisição antes de processá-la
     logger.debug(f'Requisição recebida: {request.method} {request.url}')
     logger.debug(f'Cabeçalhos: {request.headers}')
     if request.data:
@@ -18,20 +16,23 @@ def log_request_info():
 
 @app.after_request
 def log_response_info(response):
-    # Loga todos os detalhes da resposta após processá-la
     logger.debug(f'Resposta enviada: {response.status}')
     logger.debug(f'Cabeçalhos da resposta: {response.headers}')
     return response
 
 @app.route('/post', methods=['POST'])
 def post_example():
-    data = request.get_json()  # Pega os dados JSON do corpo da requisição
+    data = request.form  # Pega os dados do corpo da requisição, assumindo que são enviados como formulário
     if data:
         logger.info(f'Dados recebidos: {data}')
-        return jsonify({"message": "Dados recebidos", "data": data}), 200
+        response = jsonify({"message": "Dados recebidos", "data": data})
+        response.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+        return response, 200
     else:
         logger.warning(f'Erro: Nenhum dado enviado. Dados recebidos: {request.data}')
-        return jsonify({"message": "Nenhum dado enviado"}), 400
+        response = jsonify({"message": "Nenhum dado enviado"})
+        response.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+        return response, 400
 
 if __name__ == '__main__':
     app.run(debug=True)
